45832e1c49bca819b027c9054ae7a22c8e97133d
GeorgeKalandadze <kalandadzeg777@gmail.com>
2023-02-04 14:31:10 +0400

    add education page

--- src/pages/EducationPage/EducationPage.style.tsx
+++ src/pages/EducationPage/EducationPage.style.tsx
@@ -22,5 +22,34 @@ justify-content:space-between;
 export const ButtonsContainer = styled.div`
 margin-top:200px;
 display:flex;
-justify-content:space-between
+justify-content:space-between;
+`
+export const StyldeDropdownContainer = styled.div`
+margin-top:40px;
+display:flex;
+flex-direction:column;
+gap:10px;
+`
+
+export const StyledLabel = styled.label`
+font-style: normal;
+font-weight: 600;
+font-size: 16px;
+line-height: 21px; 
+`
+
+export const StyledDropdown = styled.select`
+width: 371px;
+background: #FFFFFF;
+border: 1px solid #BCBCBC;
+border-radius: 4px;
+padding:12px 15px;
+font-weight: 400;
+font-size: 16px;
+line-height: 21px;
+color: rgba(0, 0, 0, 0.6);
+`
+
+export const StyledOptions = styled.option`
+    
 `
--- src/pages/EducationPage/EducationPage.tsx
+++ src/pages/EducationPage/EducationPage.tsx
@@ -1,10 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Button } from '../../components/Button/Button'
 import Header from '../../components/Header/Header'
 import InputGroup from '../../components/InputGroup/InputGroup'
 import Resume from '../../components/Resume/Resume'
-import { EducationContainer, EducationForm, FlexedDiv } from './EducationPage.style'
+import TextareaGroup from '../../components/TextareaGroup/TextareaGroup'
+import { ButtonsContainer, EducationContainer, EducationForm, FlexedDiv, StyldeDropdownContainer, StyledDropdown, StyledLabel, StyledOptions } from './EducationPage.style'
 
 const EducationPage = () => {
+    const [count, setCount] = useState(0);
+
+    const handleClick = () => {
+      setCount(count + 1);
+    };
+
   return (
     <FlexedDiv>
         <EducationForm>
@@ -16,7 +24,60 @@ const EducationPage = () => {
                     hint='მინიმუმ 2 სიმბოლო'
                     inputType='text'
                 />
+                <FlexedDiv>
+                <StyldeDropdownContainer>
+                        <StyledLabel>ხარისხი</StyledLabel>
+                        <StyledDropdown >
+                            <StyledOptions defaultValue={""} disabled selected hidden>აირჩიეთ ხარისხი</StyledOptions>
+                            <StyledOptions value="1">აირჩიეთ rame</StyledOptions>
+                            <StyledOptions value="2">აირჩიეთ some</StyledOptions>
+                        </StyledDropdown>
+                    </StyldeDropdownContainer>
+                    <InputGroup
+                        label='დამთავრების რიცხვი'
+                        inputType='date'
+                        width='370px'
+                    />
+                </FlexedDiv>
+                <TextareaGroup
+                    label='აღწერა'
+                    placeholder='განათლების აღწერა'
+                />
             </EducationContainer>
+            {[...Array(count)].map((_,i) => (
+                <EducationContainer key={i}>
+                    <InputGroup
+                        label='სასწავლებელი'
+                        placeHolder='სასწავლებელი'
+                        hint='მინიმუმ 2 სიმბოლო'
+                        inputType='text'
+                    />
+                    <FlexedDiv>
+                    <StyldeDropdownContainer>
+                            <StyledLabel>ხარისხი</StyledLabel>
+                            <StyledDropdown >
+                                <StyledOptions defaultValue={""} disabled selected hidden>აირჩიეთ ხარისხი</StyledOptions>
+                                <StyledOptions value="1">აირჩიეთ rame</StyledOptions>
+                                <StyledOptions value="2">აირჩიეთ some</StyledOptions>
+                            </StyledDropdown>
+                        </StyldeDropdownContainer>
+                        <InputGroup
+                            label='დამთავრების რიცხვი'
+                            inputType='date'
+                            width='370px'
+                        />
+                    </FlexedDiv>
+                    <TextareaGroup
+                        label='აღწერა'
+                        placeholder='განათლების აღწერა'
+                    />
+                </EducationContainer>
+            ))}
+             <Button bgColor='#62A1EB' onClick={handleClick}>მეტი განათლების დამატება</Button>
+            <ButtonsContainer>
+                <Button bgColor='#6B40E3;' pdng='10px 35px'>უკან</Button>
+                <Button bgColor='#6B40E3;' pdng='10px 35px'>შემდეგი</Button>
+            </ButtonsContainer>
         </EducationForm>
         <Resume/>
     </FlexedDiv>
